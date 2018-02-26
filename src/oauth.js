9ce46e100ed9c2fdb3b954bdcbd67f8fcf07835b
Forest Gafford <forestg@imagemattersllc.com>
2018-02-26 13:31:28 -0500

    Fix bug that did not properly scramble redirect urls passed to node-gpoauth

--- src/oauth.js
+++ src/oauth.js
@@ -184,7 +184,7 @@ module.exports = function(app, userConf) {
     const authURL = CONFIG.IDP_BASE_URL +
                     CONFIG.IDP_AUTH_URL +
                     `?response_type=code` +
-                    `&redirect_uri=${CONFIG.APP_BASE_URL}/authtoken/${redirectURL}` +
+                    `&redirect_uri=` + encodeURIComponent(`${CONFIG.APP_BASE_URL}/authtoken/${redirectURL}`) +
                     `&scope=read` +
                     `&client_id=${CONFIG.APP_ID}` +
                     (req.query.sso ? '&sso=true' : '');
